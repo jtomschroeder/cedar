c2cb3be328d0798e9e3a91f5745a7e919f4e46bd
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-07-12 14:31:49 -0500

    Initial definition of changeset.

--- dom/src/main.rs
+++ dom/src/main.rs
@@ -75,26 +75,46 @@ fn zip<I, J>(i: I, j: J) -> Zip<I::IntoIter, J::IntoIter>
     }
 }
 
-fn diff<T>(t: &[Box<Node<T>>], u: &[Box<Node<T>>], level: usize)
-    where T: fmt::Debug
+// TODO: turn `level` into `path`
+
+fn diff<T>(old: &[Box<Node<T>>], new: &[Box<Node<T>>], level: usize)
+    where T: fmt::Debug + PartialEq
 {
-    let display = |t: &[Box<Node<T>>]| {
-        t.iter()
-            .map(|n| format!("{:?}", n.value))
-            .collect::<Vec<_>>()
-    };
+    // -      if `old` doesn't exist: CREATE new
+    // - else if `new` doesn't exist: REMOVE old
+    // - else if old.type != new.type: REPLACE old with new
+    // - else    update properties and keep going
 
-    println!("{:?} :: {:?} :: {:?}", display(t), display(u), level);
+    // let display = |t: &[Box<Node<T>>]| {
+    //     t.iter()
+    //         .map(|n| format!("{:?}", n.value))
+    //         .collect::<Vec<_>>()
+    // };
+    // println!("{:?} :: {:?} :: {:?}", display(old), display(new), level);
 
-    // for (t, u) in t.iter().zip(u) {
-    //     diff(&t.children, &u.children, level + 1);
-    // }
-
-    for pair in zip(t, u) {
+    for pair in zip(old, new) {
         match pair {
-            Pair::Left(t) => diff(&t.children, &[], level + 1),
-            Pair::Both(t, u) => diff(&t.children, &u.children, level + 1),
-            Pair::Right(u) => diff(&[], &u.children, level + 1),
+            Pair::Left(t) => {
+                println!("Delete {:?} @ {}", t.value, level);
+            }
+
+            Pair::Both(t, u) => {
+                // if t.type != u.type => replace u with t
+                // else if t != u (properties changes) => update and diff children
+                // else (if t == u) diff children
+
+                // TODO: compare types (don't diff children if 'replace')
+
+                if t.value != u.value {
+                    println!("Update {:?} with {:?} @ {}", t.value, u.value, level);
+                }
+
+                diff(&t.children, &u.children, level + 1)
+            }
+
+            Pair::Right(u) => {
+                println!("Create {:?} @ {}", u.value, level);
+            }
         }
     }
 }
@@ -102,23 +122,13 @@ fn diff<T>(t: &[Box<Node<T>>], u: &[Box<Node<T>>], level: usize)
 fn patch() {}
 
 fn main() {
-    // println!("Hello, world!");
+    let t = node![ 
+        0 => node![1], 
+             node![2 => node![3] 
+        ] 
+    ];
 
-    // let arena = typed_arena::Arena::new();
-
-    // let a = arena.alloc(arena_tree::Node::new(Object::Div));
-    // let b = arena.alloc(arena_tree::Node::new(Object::Div));
-
-    // a.append(b);
-
-    // for node in a.descendants() {
-    //     println!("{:?}", node.data);
-    // }
-
-    let t = node![ 0 => node![1], node![2 => node![3] ] ];
     let u = node![1];
-
-    // println!("{:#?}", (t, u));
 
     diff(&[Box::new(t)], &[Box::new(u)], 0);
 }
