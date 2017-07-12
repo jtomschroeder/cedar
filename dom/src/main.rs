1a23801aebda2ddf9ca0670302185edbb16a957e
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-07-11 23:35:23 -0500

    Tree diffing.

--- dom/src/main.rs
+++ dom/src/main.rs
@@ -2,11 +2,29 @@
 extern crate typed_arena;
 extern crate arena_tree;
 
-#[derive(Clone, Debug)]
-enum Object {
-    Div,
-    Button,
-    Text(String),
+use std::fmt;
+
+#[derive(Debug)]
+struct Node<T> {
+    value: T,
+    children: Vec<Box<Node<T>>>,
+}
+
+macro_rules! node {
+    ($v:expr) => {
+        Node {
+            value: $v,
+            children: vec![]
+        }
+    };
+
+    ( $v:expr => $( $c:expr ),* ) => {{
+        let mut children = vec![];
+
+        $( children.push(Box::new($c)); )*
+
+        Node { value: $v, children }
+    }};
 }
 
 // Path :: XPath-like location into tree
@@ -21,21 +39,39 @@ enum Object {
 // `diff(old, new, Path::Root)`
 //
 
-fn diff() {}
+// Iterator Zip with both
+// enum {
+//     Left(T), Both(T, U), Right(U)
+// }
+
+fn diff<T>(t: &[Box<Node<T>>], u: &[Box<Node<T>>], level: usize)
+    where T: fmt::Debug
+{
+    println!("{:?} :: {:?} :: {:?}", t, u, level);
+
+    // for ()
+}
 
 fn patch() {}
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    let arena = typed_arena::Arena::new();
+    // let arena = typed_arena::Arena::new();
 
-    let a = arena.alloc(arena_tree::Node::new(Object::Div));
-    let b = arena.alloc(arena_tree::Node::new(Object::Div));
+    // let a = arena.alloc(arena_tree::Node::new(Object::Div));
+    // let b = arena.alloc(arena_tree::Node::new(Object::Div));
 
-    a.append(b);
+    // a.append(b);
 
-    for node in a.descendants() {
-        println!("{:?}", node.data);
-    }
+    // for node in a.descendants() {
+    //     println!("{:?}", node.data);
+    // }
+
+    let t = node![ 0 => node![1], node![2] ];
+    let u = node![1];
+
+    // println!("{:#?}", (t, u));
+
+    diff(&[Box::new(t)], &[Box::new(u)], 0);
 }
