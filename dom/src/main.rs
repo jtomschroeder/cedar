c31f810e321a343e528b03c8e0b0db3a81b63370
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-07-12 14:19:16 -0500

    Traverse trees.

--- dom/src/main.rs
+++ dom/src/main.rs
@@ -1,6 +1,6 @@
 
-extern crate typed_arena;
-extern crate arena_tree;
+// extern crate typed_arena;
+// extern crate arena_tree;
 
 use std::fmt;
 
@@ -39,17 +39,64 @@ macro_rules! node {
 // `diff(old, new, Path::Root)`
 //
 
-// Iterator Zip with both
-// enum {
-//     Left(T), Both(T, U), Right(U)
-// }
+enum Pair<T, U> {
+    Left(T),
+    Both(T, U),
+    Right(U),
+}
+
+struct Zip<I, J> {
+    i: I,
+    j: J,
+}
+
+impl<I, J> Iterator for Zip<I, J>
+    where I: Iterator,
+          J: Iterator
+{
+    type Item = Pair<I::Item, J::Item>;
+    fn next(&mut self) -> Option<Self::Item> {
+        match (self.i.next(), self.j.next()) {
+            (Some(i), Some(j)) => Some(Pair::Both(i, j)),
+            (Some(i), _) => Some(Pair::Left(i)),
+            (_, Some(j)) => Some(Pair::Right(j)),
+            _ => None,
+        }
+    }
+}
+
+fn zip<I, J>(i: I, j: J) -> Zip<I::IntoIter, J::IntoIter>
+    where I: IntoIterator,
+          J: IntoIterator
+{
+    Zip {
+        i: i.into_iter(),
+        j: j.into_iter(),
+    }
+}
 
 fn diff<T>(t: &[Box<Node<T>>], u: &[Box<Node<T>>], level: usize)
     where T: fmt::Debug
 {
-    println!("{:?} :: {:?} :: {:?}", t, u, level);
+    let display = |t: &[Box<Node<T>>]| {
+        t.iter()
+            .map(|n| format!("{:?}", n.value))
+            .collect::<Vec<_>>()
+    };
 
-    // for ()
+    println!("{:?} :: {:?} :: {:?}", display(t), display(u), level);
+
+    // for (t, u) in t.iter().zip(u) {
+    //     diff(&t.children, &u.children, level + 1);
+    // }
+
+    for pair in zip(t, u) {
+        match pair {
+            Pair::Left(t) => diff(&t.children, &[], level + 1),
+            Pair::Both(t, u) => diff(&t.children, &u.children, level + 1),
+            Pair::Right(u) => diff(&[], &u.children, level + 1),
+        }
+    }
 }
 
 fn patch() {}
@@ -68,7 +115,7 @@ fn main() {
     //     println!("{:?}", node.data);
     // }
 
-    let t = node![ 0 => node![1], node![2] ];
+    let t = node![ 0 => node![1], node![2 => node![3] ] ];
     let u = node![1];
 
     // println!("{:#?}", (t, u));
