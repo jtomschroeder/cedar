dd2398d97ce1e11d1f2a4c3bfc419ed7c414b630
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-07-14 16:24:11 -0500

    Cleanup.

--- src/cocoa/label.rs
+++ src/cocoa/label.rs
@@ -14,7 +14,7 @@ pub struct Label {
 impl Label {
     pub fn new() -> Self {
         unsafe {
-            let string = NSString::alloc(nil).init_str("TEXT");
+            let string = NSString::alloc(nil).init_str("");
 
             let label: id = msg_send![class("NSTextField"), alloc];
             let label: id = msg_send![label, init];
