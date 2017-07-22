55ca54ad47d01be33705e8151e65a8c32e0ccdec
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-07-22 11:01:50 -0500

    Support linux CI build (#20)
    * Configure linux-specific build via Cargo.
    * Install gtk-dev dependencies on Linux [travis-ci]

--- src/lib.rs
+++ src/lib.rs
@@ -3,31 +3,31 @@ extern crate crossbeam;
 
 // --- macOS ---
 
-#[cfg(all(target_os = "macos", not(feature = "gtk3")))]
+#[cfg(all(target_os = "macos", not(feature = "gtk")))]
 #[macro_use]
 extern crate objc;
-#[cfg(all(target_os = "macos", not(feature = "gtk3")))]
+#[cfg(all(target_os = "macos", not(feature = "gtk")))]
 extern crate cocoa;
-#[cfg(all(target_os = "macos", not(feature = "gtk3")))]
+#[cfg(all(target_os = "macos", not(feature = "gtk")))]
 extern crate core_graphics;
 
-#[cfg(all(target_os = "macos", not(feature = "gtk3")))]
+#[cfg(all(target_os = "macos", not(feature = "gtk")))]
 #[path = "cocoa/mod.rs"]
 mod cacao;
 
-#[cfg(all(target_os = "macos", not(feature = "gtk3")))]
+#[cfg(all(target_os = "macos", not(feature = "gtk")))]
 pub use cacao::program;
 
 // --- gtk ---
 
-#[cfg(any(feature = "gtk3", not(target_os = "macos")))]
+#[cfg(any(feature = "gtk", not(target_os = "macos")))]
 extern crate gtk;
 
-#[cfg(any(feature = "gtk3", not(target_os = "macos")))]
+#[cfg(any(feature = "gtk", not(target_os = "macos")))]
 #[path = "gtk/mod.rs"]
 mod gtk3;
 
-#[cfg(any(feature = "gtk3", not(target_os = "macos")))]
+#[cfg(any(feature = "gtk", not(target_os = "macos")))]
 pub use gtk3::program;
 
 // --- common ---
