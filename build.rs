20868f0fb5b68c7bd0addb424ba580b4823dad76
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-10-10 21:17:44 -0500

    macOS build

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 
 extern crate cc;
 
+use std::env;
+
 fn main() {
     cc::Build::new()
         .flag("-std=c11")
@@ -10,12 +12,17 @@ fn main() {
         .include("yoga/lib")
         .compile("libyoga.a");
 
-    cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++14")
-        .file("cocoa/lib/cocoa.mm")
-        .include("cocoa/ext")
-        .compile("libcedar-cocoa.a");
+    let target = env::var("TARGET").unwrap();
+    let gtk = env::var("CARGO_FEATURE_GTK").is_ok();
 
-    println!("cargo:rustc-link-lib=framework=Cocoa");
+    if target.contains("apple") && !gtk {
+        cc::Build::new()
+            .cpp(true)
+            .flag("-std=c++14")
+            .file("cocoa/lib/cocoa.mm")
+            .include("cocoa/ext")
+            .compile("libcedar-cocoa.a");
+
+        println!("cargo:rustc-link-lib=framework=Cocoa");
+    }
 }
