8c4f909c9a04f51e9e8f9ce86475cf6d36f25059
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-11-01 12:01:27 -0500

    Command line args for cedar-tool

--- tool/src/bin/cedar.rs
+++ tool/src/bin/cedar.rs
@@ -1,6 +1,7 @@
 
 #[macro_use]
 extern crate error_chain;
+extern crate clap;
 
 use std::fs;
 use std::env;
@@ -25,19 +26,59 @@ error_chain! {
     errors {}
 }
 
-fn run() -> Result<()> {
-    let home = env::var("HOME").unwrap();
+enum Command {
+    Setup { force: bool },
+    Run { release: bool },
+}
 
-    // TODO: disable check with --force
-    if Path::new(&format!("{}/.cedar", home)).exists() {
-        println!("Already setup!");
-        return Ok(());
+fn args() -> Option<Command> {
+    use clap::{Arg, App, SubCommand};
+
+    let matches = App::new("cedar")
+        .version("0.0")
+        .author("Tom Schroeder")
+        .about("TODO")
+        .subcommand(SubCommand::with_name("setup").about("TODO").arg(
+            Arg::with_name("force").long("force"),
+        ))
+        .subcommand(SubCommand::with_name("run").about("TODO").arg(
+            Arg::with_name("release").long("release"),
+        ))
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("setup") {
+        return Some(Command::Setup { force: matches.is_present("force") });
     }
 
-    fs::create_dir_all(&format!("{}/.cedar/lib", home))?;
+    if let Some(matches) = matches.subcommand_matches("run") {
+        return Some(Command::Run { release: matches.is_present("release") });
+    }
 
-    // TODO: copy CEF.framework into .cedar/lib
-    // TODO: install_name_tool -id "{{CEF}}/Chromium Embedded Framework" "{{CEF}}/Chromium Embedded Framework"
+    None
+}
+
+fn run() -> Result<()> {
+    let command = args().unwrap();
+
+    let home = env::var("HOME").unwrap();
+
+    match command {
+        Command::Setup { force } => {
+            if !force && Path::new(&format!("{}/.cedar", home)).exists() {
+                println!("Already setup!");
+                return Ok(());
+            }
+
+            fs::create_dir_all(&format!("{}/.cedar/lib", home))?;
+
+            // TODO: copy CEF.framework into .cedar/lib
+            // TODO: install_name_tool -id "{{CEF}}/Chromium Embedded Framework" "{{CEF}}/Chromium Embedded Framework"
+        }
+
+        Command::Run { release } => {
+            // TODO: package .app
+        }
+    }
 
     Ok(())
 }
