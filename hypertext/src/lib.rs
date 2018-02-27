f87b5b827b1c52eb22636ded92e0b63dabb39aee
Tom Schroeder <j.tom.schroeder@gmail.com>
2018-02-26 21:20:40 -0600

    Cleanup.

--- hypertext/src/lib.rs
+++ hypertext/src/lib.rs
@@ -1,25 +1,25 @@
 #![allow(dead_code)]
 #![feature(proc_macro)]
 
+extern crate proc_macro2 as pm2;
 extern crate proc_macro;
-extern crate proc_macro2;
 
 use proc_macro::TokenStream;
 
-#[macro_use]
-extern crate failure;
+//#[macro_use]
+//extern crate failure;
 
 #[macro_use]
 extern crate quote;
-extern crate syn;
+//extern crate syn;
 
 mod parser;
 
-#[derive(Fail, Debug)]
-enum Error {
-    #[fail(display = "Failed to lex.")] Lex(proc_macro::LexError),
-    #[fail(display = "Failed to parse.")] Parse(syn::synom::ParseError),
-}
+//#[derive(Fail, Debug)]
+//enum Error {
+//    #[fail(display = "Failed to lex.")] Lex(proc_macro::LexError),
+//    #[fail(display = "Failed to parse.")] Parse(syn::synom::ParseError),
+//}
 
 #[proc_macro]
 pub fn hypertext(input: TokenStream) -> TokenStream {
@@ -29,13 +29,13 @@ pub fn hypertext(input: TokenStream) -> TokenStream {
     dom.render().into()
 }
 
-fn parse<T: syn::synom::Synom>(input: &str) -> Result<T, Error> {
-//    println!("input: {}", input);
-    input
-        .parse()
-        .map_err(Error::Lex)
-        .and_then(|input| syn::parse(input).map_err(Error::Parse))
-}
+//fn parse<T: syn::synom::Synom>(input: &str) -> Result<T, Error> {
+//    //    println!("input: {}", input);
+//    input
+//        .parse()
+//        .map_err(Error::Lex)
+//        .and_then(|input| syn::parse(input).map_err(Error::Parse))
+//}
 
 impl parser::Element {
     fn render(self) -> quote::Tokens {
@@ -59,9 +59,7 @@ impl parser::Element {
             }
 
             parser::Element::Block(block) => {
-//                let block: syn::Expr = parse(&block).unwrap();
-                let block: proc_macro2::TokenStream = block.parse().unwrap();
-
+                let block: pm2::TokenStream = block.parse().unwrap();
                 quote! { ::cedar::dom::object(#block) }
             }
         }
@@ -71,10 +69,9 @@ impl parser::Element {
 impl parser::Attribute {
     fn render(self) -> quote::Tokens {
         // TODO: for attrs other than 'click', use 'attr()' method
-
-        let name: syn::Ident = parse(&self.name).unwrap();
-//        let block: syn::ExprCall = parse(&self.block).unwrap();
-        let block: proc_macro2::TokenStream = self.block.parse().unwrap();
+        
+        let name: pm2::TokenStream = self.name.parse().unwrap();
+        let block: pm2::TokenStream = self.block.parse().unwrap();
 
         quote! { .#name(#block) }
     }
